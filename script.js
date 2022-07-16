fec5ebd929c861bc2573e5dadb0f68fb4dec6310
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-07-16 23:16:07 +0300

    Added game function that allows user to play the game five rounds and then gives the winner

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Select randomly an option
 const getComputerChoice = () => {
     let options = ['rock','paper', 'scissors']; 
@@ -11,35 +10,59 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return `ROW. (Your answer = ${playerSelection}, computer = ${computerSelection})`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You lost. Paper covers rock";
+        return "You lost Paper covers rock";
     }
       else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You won. Rock beats scissors"
+        return "You won Rock beats scissors"
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You won. Scissors cuts paper ";
+        return "You won Scissors cuts paper";
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You lost. Rock beats scissors";
+        return "You lost Rock beats scissors";
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return  "You won. Paper covers rock";
+        return  "You won Paper covers rock";
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You lost. Scissors cuts paper';
+        return 'You lost Scissors cuts paper';
     }
      else {
         return 'Enter rock,paper or scissors';
     }
   }
-  
-  const playerSelection = prompt("Enter rock, paper or scissors").toLowerCase().trim();
-  const computerSelection = getComputerChoice();
 
-  //Play 5 times
-  function game() {
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+
+//Play the game five times and see who is the winner computer or you
+    let computerScore = 0;
+    let yourScore = 0;
+
+function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(playerSelection, computerSelection);
-    }
-  }
-console.log(game())
+        let result = playRound(prompt('Enter paper, scissors or rock').trim().toLowerCase(), getComputerChoice());
+           
+            if (result  === 'You won Rock beats scissors' ||
+                result === "You won Scissors cuts paper" || 
+                result === "You won Paper covers rock") {
+                     yourScore +=  1;
+            }  else if (result  === "You lost Rock beats scissors" ||
+                result === "You lost Paper covers rock" || 
+                result === 'You lost Scissors cuts paper') {
+                    computerScore += 1;
+            } 
+              console.log(result)
+    }  
+        if (computerScore > yourScore) {
+            console.log("You lost");
+        } else if (yourScore > computerScore) {
+             console.log("You won");
+        } else {
+            console.log('Equal ')
+        }
+}
+
+      game()
+
+  
